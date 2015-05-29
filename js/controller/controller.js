1dafea192e6a798b398bc4a9afa3d0d6fb00f5ac
Andrea <a.g.baldan@gmail.com>
2015-05-29 17:18:17 +0200

    mah

--- js/controller/controller.js
+++ js/controller/controller.js
@@ -7,12 +7,12 @@ db.transaction(function(tx) {
 	// tx.executeSql('DROP TABLE CUSTOMERS');
 	// tx.executeSql('DROP TABLE BOOKS');
 	// development
-	tx.executeSql('CREATE TABLE IF NOT EXISTS '+
-		'STORE (id INTEGER PRIMARY KEY ASC, Materia TEXT, Isbn TEXT UNIQUE, Autore TEXT, Titolo TEXT, Volume INTEGER, Casa TEXT, Prezzo REAL)');
-	tx.executeSql('CREATE TABLE IF NOT EXISTS '+
-		'CUSTOMERS (id INTEGER PRIMARY KEY ASC, Nome TEXT, Telefono TEXT)');
-	tx.executeSql('CREATE TABLE IF NOT EXISTS '+
-		'BOOKS (id INTEGER PRIMARY KEY ASC, Isbn TEXT, IdCustomer INTEGER, Discount INTEGER, Sold INTEGER)');
+	// tx.executeSql('CREATE TABLE IF NOT EXISTS '+
+	// 	'STORE (id INTEGER PRIMARY KEY ASC, Materia TEXT, Isbn TEXT UNIQUE, Autore TEXT, Titolo TEXT, Volume INTEGER, Casa TEXT, Prezzo REAL)');
+	// tx.executeSql('CREATE TABLE IF NOT EXISTS '+
+	// 	'CUSTOMERS (id INTEGER PRIMARY KEY ASC, Nome TEXT, Telefono TEXT)');
+	// tx.executeSql('CREATE TABLE IF NOT EXISTS '+
+	// 	'BOOKS (id INTEGER PRIMARY KEY ASC, Isbn TEXT, IdCustomer INTEGER, Discount INTEGER, Sold INTEGER)');
 });
 // main controller
 usatoApp.controller('MainController', function($scope, utility, usatoAppFactory, usatoAppCustomerFactory, $location) {
@@ -20,6 +20,7 @@ usatoApp.controller('MainController', function($scope, utility, usatoAppFactory,
 	$scope.headerSrc = './tmpl/header.html';
 	// load books into scope variable
 	$scope.$on('refreshStore', function(event) {
+        $scope.store = null;
 		usatoAppFactory.get().then(function(s) {
 			$scope.store = s;
 		});
@@ -27,22 +28,25 @@ usatoApp.controller('MainController', function($scope, utility, usatoAppFactory,
 	$scope.$emit('refreshStore');
 	// load books into scope variable
 	$scope.$on('refreshBooks', function(event) {
+        $scope.books = null;
 		usatoAppFactory.books().then(function(b) {
-			$scope.books = b;
-		});
+		 	$scope.books = b;
+		});                    
 	});
 	$scope.$emit('refreshBooks');
 	// set limit to 10 results for home page
 	$scope.quantity = 10;
 	// listen for event 'refresh' and refresh customers list
 	$scope.$on('refresh', function(event) {
-		usatoAppCustomerFactory.get().then(function(c) {
+        $scope.customers = null;
+		 usatoAppCustomerFactory.get().then(function(c) {
 			$scope.customers = c;
 		});
-	});
+   	});
 	$scope.$emit('refresh');
 	// refresh solds book list
 	$scope.$on('refreshSold', function(event) {
+        $scope.soldBooks = null;
 		usatoAppCustomerFactory.sold().then(function(s) {
 			$scope.soldBooks = s;
 		});
@@ -72,11 +76,13 @@ usatoApp.controller('MainController', function($scope, utility, usatoAppFactory,
 	};
 	// return the customer from id
 	$scope.getOwner = function(id) {
-		for (var i = 0, len = $scope.customers.length; i < len; i++) {
-			if($scope.customers[i].id == id) {
-				return $scope.customers[i].Nome;
-			}
-		}
+        if(typeof $scope.customers !== 'undefined') {
+		    for (var i = 0, len = $scope.customers.length; i < len; i++) {
+			    if($scope.customers[i].id == id) {
+				    return $scope.customers[i].Nome;
+			    }
+		    }
+        } else { return 'Unknown'; }
 	};
 	// get discount for a given book
 	$scope.getDiscount = function(bk, ds) {
@@ -86,11 +92,11 @@ usatoApp.controller('MainController', function($scope, utility, usatoAppFactory,
 	$scope.sell = function(bid) {
 		db.transaction(function(tx) {
 			tx.executeSql('UPDATE BOOKS SET Sold = 1 WHERE id = ?', [bid]);
+            // reload books
+		    $scope.$emit('refreshBooks');
+            // reload sold
+		    $scope.$emit('refreshSold');
 		});
-		// reload sold
-		$scope.$emit('refreshSold');
-		// reload books
-		$scope.$emit('refreshBooks');
 	};
 });
 // customers managing and CRUD
@@ -101,28 +107,36 @@ usatoApp.controller('customersController', function($scope) {
 		db.transaction(function(tx) {
 			tx.executeSql('DELETE FROM CUSTOMERS WHERE id = ?', [identifier]);
 			tx.executeSql('DELETE FROM BOOKS WHERE IdCustomer = ?', [identifier]);
+            // reload customers
+		    $scope.$emit('refresh');
 		});
-		// reload customers
-		$scope.$emit('refresh');
 	};
 	// check if that user have sold all his books
 	$scope.allSold = function(idc) {
 		var tots = 0;
-		for (var j = 0, len = $scope.books.length; j < len; j++) {
-			if($scope.books[j].IdCustomer == idc) {
-				tots++;
-			}
-		}
+        if(typeof $scope.books !== 'undefined') {
+		    for (var j = 0, len = $scope.books.length; j < len; j++) {
+			    if($scope.books[j].IdCustomer == idc) {
+				    tots++;
+			    }
+		    }
+        } else { tots = 0; }
 		return tots > 0;
 	};
 });
 // single customer managing, access granted by id
 usatoApp.controller('showCustomerController', function($scope, $routeParams, usatoAppCustomerFactory) {
 	$scope.getCustomerById($routeParams.id);
-	usatoAppCustomerFactory.booksById($routeParams.id).then(function(r) {
-		$scope.reservedBooks = r;
-	});
+    // reload books for a user
+    $scope.$on('refreshReserved', function(event) {
+        $scope.reservedBooks = null;
+	    usatoAppCustomerFactory.booksById($routeParams.id).then(function(r) {
+		    $scope.reservedBooks = r;
+	    });
+    });
+    $scope.$emit('refreshReserved');
 	// get sold book list for a given user
+    $scope
 	usatoAppCustomerFactory.soldBooksById($routeParams.id).then(function(sb) {
 		$scope.soldBooksByMe = sb;
 	});
@@ -130,46 +144,54 @@ usatoApp.controller('showCustomerController', function($scope, $routeParams, usa
 	$scope.deleteCopy = function(idc) {
 		db.transaction(function(tx) {
 			tx.executeSql('DELETE FROM BOOKS WHERE IdCustomer = ? AND Id = ?', [$routeParams.id, idc]);
-		});
-		// reload books
-		usatoAppCustomerFactory.booksById($routeParams.id).then(function(r) {
-			$scope.reservedBooks = r;
+            $scope.$emit('refreshReserved');
 		});
 	};
 	// restore a book from sell list to unsell
 	$scope.restore = function(bid) {
 		db.transaction(function(tx) {
 			tx.executeSql('UPDATE BOOKS SET Sold = 0 WHERE IdCustomer = ? AND id = ?', [$routeParams.id, bid]);
-		});
-		// refresh general sold list
-		$scope.$emit('refreshSold');
-		// refresh sold list
-		usatoAppCustomerFactory.soldBooksById($routeParams.id).then(function(sb) {
-			$scope.soldBooksByMe = sb;
+            // refresh general sold list
+		    $scope.$emit('refreshSold');
+		    // refresh sold list
+            $scope.soldBooksByMe = null;
+		    usatoAppCustomerFactory.soldBooksById($routeParams.id).then(function(sb) {
+			    $scope.soldBooksByMe = sb;
+		    });
 		});
 	};
 	// get total cost of reserved books
 	$scope.getTotal = function() {
 		var TOT = {total: 0, dtotal: 0};
 		var rbooks = $scope.reservedBooks;
-		for(var i = 0; i < rbooks.length; i++) {
-			TOT.total += parseFloat(rbooks[i].Prezzo.replace(/,/, '.'));
-			TOT.dtotal += parseFloat(rbooks[i].Prezzo.replace(/,/, '.')) - (parseFloat(rbooks[i].Prezzo.replace(/,/, '.')) * (parseFloat(rbooks[i].Discount) * 0.01));
-		}
-		TOT.total = Math.round(TOT.total * 100) / 100;
-		TOT.dtotal = Math.round(TOT.dtotal * 100) / 100;
+        if(typeof rbooks !== 'undefined') {
+		    for(var i = 0; i < rbooks.length; i++) {
+			    TOT.total += parseFloat(rbooks[i].Prezzo.replace(/,/, '.'));
+			    TOT.dtotal += parseFloat(rbooks[i].Prezzo.replace(/,/, '.')) - (parseFloat(rbooks[i].Prezzo.replace(/,/, '.')) * (parseFloat(rbooks[i].Discount) * 0.01));
+		    }
+		    TOT.total = Math.round(TOT.total * 100) / 100;
+		    TOT.dtotal = Math.round(TOT.dtotal * 100) / 100;
+        } else {
+            TOT.total = 0;
+            TOT.dtotal = 0;
+        }
 		return TOT;
 	};
 	// get total cost of sold books
 	$scope.getTotalSold = function() {
 		var TOT = {total: 0, dtotal: 0};
 		var sbooks = $scope.soldBooksByMe;
-		for(var i = 0; i < sbooks.length; i++) {
-			TOT.total += parseFloat(sbooks[i].Prezzo.replace(/,/, '.'));
-			TOT.dtotal += parseFloat(sbooks[i].Prezzo.replace(/,/, '.')) - (parseFloat(sbooks[i].Prezzo.replace(/,/, '.')) * (parseFloat(sbooks[i].Discount) * 0.01));
-		}
-		TOT.total = Math.round(TOT.total * 100) / 100;
-		TOT.dtotal = Math.round(TOT.dtotal * 100) / 100;
+        if(typeof sbooks !== 'undefined') {
+		    for(var i = 0; i < sbooks.length; i++) {
+			    TOT.total += parseFloat(sbooks[i].Prezzo.replace(/,/, '.'));
+			    TOT.dtotal += parseFloat(sbooks[i].Prezzo.replace(/,/, '.')) - (parseFloat(sbooks[i].Prezzo.replace(/,/, '.')) * (parseFloat(sbooks[i].Discount) * 0.01));
+		    }
+		    TOT.total = Math.round(TOT.total * 100) / 100;
+		    TOT.dtotal = Math.round(TOT.dtotal * 100) / 100;
+        } else {
+            TOT.total = 0;
+            TOT.dtotal = 0;
+        }
 		return TOT;
 	};
 	// get discount for a given book
@@ -236,7 +258,7 @@ usatoApp.controller('archiveController', function($scope, utility) {
 			tx.executeSql('INSERT OR IGNORE INTO STORE (Materia, Isbn, Autore, Titolo, Volume, Casa, Prezzo) values (?, ?, ?, ?, ?, ?, ?)',
 				[book.Materia, book.Isbn, book.Autore, book.Titolo, book.Volume, book.Casa, book.Prezzo]);
 		});
-		call alert for success operation
+		// call alert for success operation
 		$('#success-alert-arc').alert();
 		$('#success-alert-arc').fadeTo(2000, 500).fadeOut(1000, function() {
 			$('#success-alert-arc').alert('close');
--- js/service/services.js
+++ js/service/services.js
@@ -1,3 +1,4 @@
+var db = openDatabase('usato', '1.0', 'Usato database', 5 * 1024 * 1024);
 usatoApp.factory('utility', function() {
 	// Download html of a URL specified page
 	return {
@@ -27,31 +28,30 @@ usatoApp.factory('usatoAppFactory', function($resource, $q) {
 					for(var i = 0; i < results.rows.length; i++) {
 						arr.push(results.rows.item(i));
 					}
-					if(arr.length > 0) deferred.resolve(arr);
-					else deferred.reject(arr);
+                    deferred.resolve(arr);
 				});
 			});
 			return deferred.promise;
 		},
 		books: function() {
 			var deferred = $q.defer();
-			db.transaction(function(tx) {
-				tx.executeSql('SELECT Isbn, id FROM BOOKS WHERE Sold = 0', [], function(tx, results) {
-					var r = results.rows;
-					var arr = [];
-					for(var i = 0; i < r.length; i++) {
-						tx.executeSql('SELECT s.Titolo, s.Materia, s.Autore, s.Volume, s.Casa, s.Isbn, s.Prezzo, b.id, b.Discount, b.IdCustomer FROM STORE s, BOOKS b WHERE s.Isbn = ? AND b.Isbn = ? AND b.id = ?', [r.item(i).Isbn, r.item(i).Isbn, r.item(i).id], function(tx, results) {
-							for(var j = 0; j < results.rows.length; j++) {
-								arr.push(results.rows.item(j));
-							}
-							if (arr.length > 0) deferred.resolve(arr);
-							else deferred.reject(arr);
-						});
-					}
-				});
-			});
-			return deferred.promise;
-		},
+            var arr = new Array();
+            db.transaction(function(tx) {
+                function checkResults(r) {
+                    for(var i = 0; i < r.length; i++) {
+                        tx.executeSql('SELECT s.Titolo, s.Materia, s.Autore, s.Volume, s.Casa, s.Isbn, s.Prezzo, b.id, b.Discount, b.IdCustomer FROM STORE s, BOOKS b WHERE s.Isbn = ? AND b.Isbn = ? AND b.id = ?', [r.item(i).Isbn, r.item(i).Isbn, r.item(i).id], function(tx, resuz) {
+                            for(var j = 0; j < resuz.rows.length; j++) 
+                                arr.push(resuz.rows.item(j));
+                            deferred.resolve(arr);
+                        });
+                    }
+                }
+			    tx.executeSql('SELECT Isbn, id FROM BOOKS WHERE Sold = 0', [], function(tx, resuz) {
+                    checkResults(resuz.rows);
+                });
+            });
+            return deferred.promise;
+	    },
 		copiesById: function(isbn) {
 			var deferred = $q.defer();
 			db.transaction(function(tx) {
@@ -75,69 +75,68 @@ usatoApp.factory('usatoAppCustomerFactory', function($resource, $q) {
 					for(var i = 0; i < results.rows.length; i++) {
 						arr.push(results.rows.item(i));
 					}
-					if(arr.length > 0) deferred.resolve(arr);
-					else deferred.reject(arr);
+                    deferred.resolve(arr);
 				});
 			});
 			return deferred.promise;
 		},
 		sold: function() {
 			var deferred = $q.defer();
-			db.transaction(function(tx) {
-				tx.executeSql('SELECT * FROM BOOKS WHERE Sold = 1', [], function(tx, results) {
-					var r = results.rows;
-					var arr = [];
-					for(var i = 0; i < r.length; i++) {
-						tx.executeSql('SELECT s.Titolo, s.Materia, s.Autore, s.Volume, s.Casa, s.Isbn, s.Prezzo, b.id, b.Discount, b.IdCustomer FROM STORE s, BOOKS b WHERE s.Isbn = ? AND b.Isbn = ? AND b.id = ?', [r.item(i).Isbn, r.item(i).Isbn, r.item(i).id], function(tx, results) {
-							for(var j = 0; j < results.rows.length; j++) {
-								arr.push(results.rows.item(j));
-							}
-							if(arr.length > 0) deferred.resolve(arr);
-							else deferred.reject(arr);
-						});
-					}
-				});
-			});
-			return deferred.promise;
+            var arr = new Array();
+            db.transaction(function(tx) {
+                function checkResults(r) {
+                    for(var i = 0; i < r.length; i++) {
+                        tx.executeSql('SELECT s.Titolo, s.Materia, s.Autore, s.Volume, s.Casa, s.Isbn, s.Prezzo, b.id, b.Discount, b.IdCustomer FROM STORE s, BOOKS b WHERE s.Isbn = ? AND b.Isbn = ? AND b.id = ?', [r.item(i).Isbn, r.item(i).Isbn, r.item(i).id], function(tx, resuz) {
+                            for(var j = 0; j < resuz.rows.length; j++) 
+                                arr.push(resuz.rows.item(j));
+                            deferred.resolve(arr);
+                        });
+                    }
+                }
+			    tx.executeSql('SELECT Isbn, id FROM BOOKS WHERE Sold = 1', [], function(tx, resuz) {
+                    checkResults(resuz.rows);
+                });
+            });
+            return deferred.promise;
 		},
 		booksById: function(id) {
-			var deferred = $q.defer();
-			db.transaction(function(tx) {
-				tx.executeSql('SELECT Isbn, id FROM BOOKS WHERE IdCustomer = ?', [id], function(tx, results) {
-					var r = results.rows;
-					var arr = [];
-					for(var i = 0; i < r.length; i++) {
-						tx.executeSql('SELECT s.Titolo, s.Autore, s.Casa, s.Isbn, s.Materia, s.Volume, s.Prezzo, b.Discount, b.IdCustomer, b.id FROM STORE s, BOOKS b WHERE s.Isbn = ? AND b.Isbn= ? AND b.IdCustomer = ? AND b.id = ?', [r.item(i).Isbn, r.item(i).Isbn, id, r.item(i).id], function(tx, results) {
-							for(var j = 0; j < results.rows.length; j++) {
-								arr.push(results.rows.item(j));
-							}
-							if(arr.length > 0) deferred.resolve(arr);
-							else deferred.reject(arr);
-						});
-					}
-				});
-			});
-			return deferred.promise;
-		},
+            var deferred = $q.defer();
+            var arr = new Array();
+            db.transaction(function(tx) {
+                function checkResults(r) {
+                    for(var i = 0; i < r.length; i++) {
+                        tx.executeSql('SELECT s.Titolo, s.Autore, s.Casa, s.Isbn, s.Materia, s.Volume, s.Prezzo, b.Discount, b.IdCustomer, b.id FROM STORE s, BOOKS b WHERE s.Isbn = ? AND b.Isbn= ? AND b.IdCustomer = ? AND b.id = ?', [r.item(i).Isbn, r.item(i).Isbn, id, r.item(i).id], function(tx, resuz) {
+                            for(var j = 0; j < resuz.rows.length; j++) 
+                                arr.push(resuz.rows.item(j));
+                            deferred.resolve(arr);
+                        });
+                    }
+                }
+			    tx.executeSql('SELECT Isbn, id FROM BOOKS WHERE IdCustomer = ?', [id], function(tx, resuz) {
+                    checkResults(resuz.rows);
+                });
+            });
+            return deferred.promise;
+        },
 		soldBooksById: function(id) {
 			var deferred = $q.defer();
-			db.transaction(function(tx) {
-				tx.executeSql('SELECT Isbn, id FROM BOOKS WHERE IdCustomer = ? AND Sold = 1', [id], function(tx, results) {
-					var r = results.rows;
-					var arr = [];
-					for (var i = 0, len = r.length; i < len; i++) {
-						tx.executeSql('SELECT s.Isbn, s.Autore, s.Casa, s.Titolo, s.Volume, s.Prezzo, s.Materia, b.id, b.Discount, b.IdCustomer FROM STORE s, BOOKS b WHERE s.Isbn = ? AND b.Isbn = ? AND b.IdCustomer = ? AND b.id = ?', [r.item(i).Isbn, r.item(i).Isbn, id, r.item(i).id],function(tx, results) {
-							for(var j = 0; j < results.rows.length; j++) {
-								arr.push(results.rows.item(j));
-							}
-							if(arr.length > 0) deferred.resolve(arr);
-							else deferred.reject(arr);
-						});
-					}
-				});
-			});
-			return deferred.promise;
-		},
+            var arr = [];
+            db.transaction(function(tx) {
+                function checkResults(r) {
+                    for(var i = 0; i < r.length; i++) {
+                        tx.executeSql('SELECT s.Isbn, s.Autore, s.Casa, s.Titolo, s.Volume, s.Prezzo, s.Materia, b.id, b.Discount, b.IdCustomer FROM STORE s, BOOKS b WHERE s.Isbn = ? AND b.Isbn = ? AND b.IdCustomer = ? AND b.id = ?', [r.item(i).Isbn, r.item(i).Isbn, id, r.item(i).id], function(tx, resuz) {
+                            for(var j = 0; j < resuz.rows.length; j++) 
+                                arr.push(resuz.rows.item(j));
+                            deferred.resolve(arr);
+                        });
+                    }
+                }
+			    tx.executeSql('SELECT Isbn, id FROM BOOKS WHERE IdCustomer = ? AND Sold = 1', [id], function(tx, resuz) {
+                    checkResults(resuz.rows);
+                });
+            });
+            return deferred.promise;
+        },
 		discount: function(bk, ds) {
 			return Math.round((parseFloat(bk.replace(/,/, '.')) - (parseFloat(bk.replace(/,/, '.')) * (parseFloat(ds) * 0.01))) * 100) / 100;
 		}
